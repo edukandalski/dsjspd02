3b6b5767263797cee109d971ed946ce1a210650d
Eduardo Kandalski <edukandalski@gmail.com>
2025-02-02 10:17:58 -0300

    Primeiro commit contendo spring boilerplate

---
+++ src/test/java/com/eduardokandalski/dsjspd02/Dsjspd02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardokandalski.dsjspd02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsjspd02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
